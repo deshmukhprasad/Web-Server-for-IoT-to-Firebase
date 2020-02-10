bb484a4495a72b6384192269841ba58b6b3232bc
Prasad Deshmukh <deshmukhprasad@users.noreply.github.com>
2020-02-10 17:14:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,10 +58,10 @@ def predict():
 	# yd = y.values																	#loading the pre-trained explorer
 	# exp = t.explain_instance(yd[0], predict_fn, num_features=5)						#predicting the features
 	
-	data = { 'date': timeStamp, 'air': air, 'wlev': wlev, 'hum': hum, 'wet1': wet1, 'wet2': wet2, 'temp': temp, 'soap': soap }		#data to be pushed
+	data = { 'date': timeStamp, 'air': air, 'wlev': wlev, 'hum': hum, 'wet1': wet1, 'wet2': wet2, 'temp': temp }		#data to be pushed
 	db.child(tid).child("ctdata").update(data)													# updating real time data
 	db.child(tid).child("tdata").child(timeStamp).set(data)									#querrrying database to push the data with timestamp as key
-
+	db.child(tid).child("soap").update(soap)
 	return '''<h1>The feature value is: {}</h1>'''.format(data)
 
 @app.route('/freq/', methods=['GET'])
